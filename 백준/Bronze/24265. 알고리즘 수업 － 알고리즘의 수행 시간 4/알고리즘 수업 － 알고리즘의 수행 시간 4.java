a8441c3d4129ce162d4f6b18783d01bed5131aa9
SuperCuteKitty00 <136810467+SuperCuteKitty00@users.noreply.github.com>
2025-02-18 14:33:19 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 176 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        Long n = sc.nextLong();
+        System.out.println(n*(n-1)/2);
+        System.out.println(2);
+    }
+}
