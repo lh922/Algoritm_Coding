f1465232df68a54ea8805628384d27f363d1b385
SuperCuteKitty00 <136810467+SuperCuteKitty00@users.noreply.github.com>
2025-02-25 02:40:04 +0900

    [Silver III] Title: N과 M (1), Time: 364 ms, Memory: 38152 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Main {
+  static int[] arr;
+  static boolean[] visit;
+  static StringBuilder sb = new StringBuilder();
+  
+  private static void dfs(int N, int M, int depth) {
+    if(depth == M) {
+      for(int a : arr) {
+        sb.append(a + " ");
+      }
+      sb.append("\n");
+      return;
+    }
+    
+    for(int i=1; i<=N; i++) {
+      if(!visit[i]) {
+        visit[i] = true;
+        arr[depth] = i;
+        dfs(N, M, depth+1);
+        visit[i] = false;
+      }
+    }
+    return;
+  }
+  
+  public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    
+    int N = sc.nextInt();
+    int M = sc.nextInt();
+    arr = new int[M]; visit = new boolean[N+1];
+    
+    dfs(N, M, 0);
+    System.out.println(sb);
+  }
+}
