ca5b41af6b14851943dc42cb45806a4a7a88d4d8
SuperCuteKitty00 <136810467+SuperCuteKitty00@users.noreply.github.com>
2025-03-10 14:40:12 +0900

    [Silver III] Title: 계단 오르기, Time: 104 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    
+    int[] stairs = new int[N+1];
+    int[] dp = new int[N+1];
+    for(int i=1; i<=N; i++) {
+      stairs[i] = Integer.parseInt(br.readLine());
+    }
+    
+    if(N == 1) {
+      System.out.println(stairs[1]);
+      return;
+    } else if(N == 2) {
+      System.out.println(stairs[1] + stairs[2]);
+      return;
+    }
+    
+    dp[1] = stairs[1];
+    dp[2] = stairs[1] + stairs[2];
+    dp[3] = Math.max(stairs[1], stairs[2]) + stairs[3];
+    
+    for(int i=4; i<=N; i++) {
+      dp[i] = Math.max(dp[i-2] + stairs[i], dp[i-3] + stairs[i-1] + stairs[i]);;
+    }
+    
+    System.out.println(dp[N]);
+  }
+}
