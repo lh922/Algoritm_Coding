ad07a97c97c2af64d6dd45d1f4eee3ca72b96a00
SuperCuteKitty00 <136810467+SuperCuteKitty00@users.noreply.github.com>
2025-04-02 20:50:12 +0900

    [Silver II] Title: 주식, Time: 1024 ms, Memory: 314348 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -4,27 +4,28 @@ import java.util.*;
 class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int tCase = Integer.parseInt(br.readLine());
+    int testCaseCount = Integer.parseInt(br.readLine());
     
-    for(int T=0; T<tCase; T++) {
-      int N = Integer.parseInt(br.readLine());
+    for (int T = 0; T < testCaseCount; T++) {
+      int numDays = Integer.parseInt(br.readLine());
       StringTokenizer st = new StringTokenizer(br.readLine());
-      int[] arr = new int[N];
-      int highestValue = 0;
-      long totalValue = 0;
+      int[] stockPrices = new int[numDays];
+      int maxStockPrice = 0;
+      long maxProfit = 0;
       
-      for(int i=0; i<N; i++) {
-        arr[i] = Integer.parseInt(st.nextToken());
+      for (int i = 0; i < numDays; i++) {
+        stockPrices[i] = Integer.parseInt(st.nextToken());
       }
       
-      for(int j=N-1; j>=0; j--) {
-        if(highestValue < arr[j]) highestValue = arr[j];
-        else if(arr[j] < highestValue) {
-          totalValue += highestValue - arr[j];
+      for (int j = numDays - 1; j >= 0; j--) {
+        if (maxStockPrice < stockPrices[j]) {
+          maxStockPrice = stockPrices[j]; 
+        } else {
+          maxProfit += maxStockPrice - stockPrices[j];
         }
       }
       
-      System.out.println(totalValue);
+      System.out.println(maxProfit);
     }
   }
 }
