33e4c450d9513ecf9231e57b26b4ccda39b747b2
SuperCuteKitty00 <136810467+SuperCuteKitty00@users.noreply.github.com>
2025-04-24 23:53:55 +0900

    [Silver II] Title: 종이의 개수, Time: 828 ms, Memory: 311080 KB -BaekjoonHub

---
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[][] paper;
+    static int[] count = new int[3];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        paper = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                paper[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        divide(0, 0, N);
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(count[i]);
+        }
+    }
+
+    static void divide(int x, int y, int size) {
+        if (check(x, y, size)) {
+            count[paper[x][y] + 1]++;
+            return;
+        }
+
+        int newSize = size / 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                divide(x + i * newSize, y + j * newSize, newSize);
+            }
+        }
+    }
+
+    static boolean check(int x, int y, int size) {
+        int value = paper[x][y];
+        for (int i = x; i < x + size; i++) {
+            for (int j = y; j < y + size; j++) {
+                if (paper[i][j] != value) return false;
+            }
+        }
+        return true;
+    }
+}
